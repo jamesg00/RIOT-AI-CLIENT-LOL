ef96482463c0e92434bac21b59d4cb9252e4e4a1
jamesg00 <159197184+jamesg00@users.noreply.github.com>
2025-09-20 13:12:33 -0700

    New files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const API = import.meta.env.VITE_API_BASE;
+
 
 function App() {
   const [count, setCount] = useState(0)
